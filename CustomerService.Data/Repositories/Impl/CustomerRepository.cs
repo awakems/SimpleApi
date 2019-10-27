3c8f62a8f370836d01468f20fbe54d8033587d21
awakems <explo1dms@gmail.com>
2019-10-27 22:38:08 +0200

    add data layer

---
+++ CustomerService.Data/Repositories/Impl/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerService.Data.Repositories.Impl
+{
+    public class CustomerRepository
+    {
+        : ICustomerRepository
+    {
+        TestDbEntities _context = new TestDbEntities();
+
+        public Customer GetCustomerBy(int id)
+        {
+            return _context.Customers.Find(id);
+        }
+
+        public Customer GetCustomerBy(string email)
+        {
+            return _context.Customers.FirstOrDefault(x => x.ContactEmail == email);
+        }
+
+        public Customer GetCustomerBy(int id, string email)
+        {
+            return _context.Customers.FirstOrDefault(x => x.ID == id && x.ContactEmail == email);
+        }
+
+        public void Dispose() => _context.Dispose();
+    }
+}
