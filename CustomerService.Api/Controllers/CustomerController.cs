3af43b4c1d0821503408629d7c7efc1e24c2d4e4
awakems <explo1dms@gmail.com>
2019-10-27 23:07:41 +0200

    Setup controller

--- CustomerService.Api/Controllers/CustomerController.cs
+++ CustomerService.Api/Controllers/CustomerController.cs
@@ -1,14 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using AutoMapper;
+using CustomerService.Api.Models;
+using CustomerService.Data.Services;
+using System;
+using System.Text.RegularExpressions;
 using System.Web.Http;
 
 namespace CustomerService.Api.Controllers
 {
     public class CustomerController : ApiController
     {
+        ICustomerService _customerService;
+        IMapper _mapper;
 
+        public CustomerController(ICustomerService customerService, IMapper mapper)
+        {
+            _customerService = customerService;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get customer by id from database
+        /// </summary>
+        /// <param name="customerID"> Customer id</param>
+        /// <returns>Returns Customer object without Transactions</returns>
+        [HttpGet]
+        public IHttpActionResult Get([FromUri]int customerID)
+        {
+            try
+            {
+                if (customerID <= 0) return BadRequest("Invalid Customer ID ");
+                var serviceModel = _customerService.GetCustomerBy(customerID);
+                if (serviceModel == null) return BadRequest("No inquiry criteria");
+                var result = _mapper.Map<CustomerApiModel>(serviceModel);
+                return Ok<CustomerApiModel>(result);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+
+        }
+
+        /// <summary>
+        /// Get customer by email from database
+        /// </summary>
+        /// <param name="email">Customer Email</param>
+        /// <returns>returns Customer object with one last Transactions</returns>
+        [HttpGet]
+        public IHttpActionResult Get([FromUri]string email)
+        {
+            try
+            {
+                if (!ValidEmail(email)) return BadRequest("Invalid Email");
+                var serviceModel = _customerService.GetCustomerBy(email);
+                if (serviceModel == null) return BadRequest("No inquiry criteria");
+                var result = _mapper.Map<CustomerApiModel>(serviceModel);
+                return Ok<CustomerApiModel>(result);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+
+        }
+
+        /// <summary>
+        /// Get customer by email and customerID from database
+        /// </summary>
+        /// <param name="customerID"></param>
+        /// <param name="email"></param>
+        /// <returns>returns Customer object with 5 last Transactions</returns>
+        [HttpGet]
+        public IHttpActionResult Get([FromUri] int customerID, string email)
+        {
+            try
+            {
+                if (customerID <= 0) return BadRequest("Invalid Customer ID ");
+                if (!ValidEmail(email)) return BadRequest("Invalid Email");
+                var serviceModel = _customerService.GetCustomerBy(customerID, email);
+                if (serviceModel == null) return BadRequest("No inquiry criteria");
+                var result = _mapper.Map<CustomerApiModel>(serviceModel);
+                return Ok<CustomerApiModel>(result);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+
+        }
+
+        private bool ValidEmail(string emailString)
+        {
+            if (emailString == null) return false;
+            if (emailString.Length > 25) return false;
+            bool isEmail = Regex.IsMatch(emailString, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+            return isEmail;
+        }
     }
 }
