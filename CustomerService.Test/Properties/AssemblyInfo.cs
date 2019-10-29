59d638bbc1b4fdbe47213c3e83ec96b3ed82119d
awakems <explo1dms@gmail.com>
2019-10-29 23:59:57 +0200

    add Test moduse and test to controler

---
+++ CustomerService.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomerService.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomerService.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6ff9cc9-90e6-4df8-99d5-0afed7ea5d3b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomerService.Test/TestCustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Results;
+using AutoMapper;
+using CustomerService.Api.Controllers;
+using CustomerService.Data.Repositories;
+using CustomerService.Data.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CustomerService.Test
+{
+    [TestClass]
+    public class TestCustomerController
+    {
+        [TestMethod]
+        public void PostWithIdReturnsBedRequest()
+        {
+            // Arrange
+            var mockService = new Mock<ICustomerService>();
+            var mockMapper = new Mock<IMapper>();
+            var controller = new CustomerController(mockService.Object, mockMapper.Object);
+
+            // Act
+            IHttpActionResult actionResult = controller.Post(new Api.Models.RequestModels.CustomerRequestModel { customerID = 999});
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(BadRequestErrorMessageResult));
+        }
+    }
+}
